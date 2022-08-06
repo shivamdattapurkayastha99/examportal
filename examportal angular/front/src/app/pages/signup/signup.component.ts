1d209e7fa9c542eea552acc57132c95d09f39eba
shivam datta purkayastha <shivamdatta465@gmail.com>
2022-08-06 22:45:13 +0530

    first commit

---
+++ examportal angular/front/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+  public user={
+    username:'',
+    password:'',
+    firstname:'',
+    lastname:'',
+    email:'',
+    phone:'',
+  }
+  ngOnInit(): void {
+  }
+  formSubmit(){
+    alert("submit");
+    console.log(this.user);
+    if (this.user.username==""||this.user.username==null) {
+      alert('user is required')
+      return;
+      
+    }
+    
+
+  }
+}
